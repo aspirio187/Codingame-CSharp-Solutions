64a5413bb945cd199544e91785f1f74598f084f0
Aspirio <soultan_98@hotmail.com>
2022-01-21 16:28:46 +0100

    feat: Added OptimalChange

---
+++ Codingame-CSharp/OptimalChange.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codingame_CSharp
+{
+    public class Change
+    {
+        public long Bill10 { get; set; } = 0;
+        public long Bill5 { get; set; } = 0;
+        public long Coin2 { get; set; } = 0;
+    }
+
+    public static class OptimalChange
+    {
+        public static Change GetOptimalChange(long s)
+        {
+            Change change = new Change();
+
+            if (s < 5 && s != 2) return change;
+
+            long m = s % 10;
+            if (m != 0 && m < 5 && m != 2)
+                change.Bill10 = (s / 10) - 1;
+            else
+                change.Bill10 = s / 10;
+
+            long x = (s - (change.Bill10 * 10)) % 2;
+            if (x > 0) change.Bill5 = x;
+            change.Coin2 = (s - (change.Bill10 * 10) - (change.Bill5 * 5)) / 2;
+
+            return change;
+        }
+    }
+}
